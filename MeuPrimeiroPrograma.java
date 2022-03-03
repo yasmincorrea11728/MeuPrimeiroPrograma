1c7d605447b2336299b1c59f9765fd699b67a2b7
unknown <yasmincorrea1235@gmail.com>
2022-03-03 19:32:08 -0300

    Meu Primeiro Programa

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,8 @@
+public class MeuPrimeiroPrograma{
+	public static void main(String args[]){
+		System.out.println("Laboratorio de Programacao 2 - LP2");
+		System.out.println("2 Ano Informatica");
+		System.out.println("Seja bem vindo");
+		System.out.println("Vamos aprender java.");
+	}
+}
